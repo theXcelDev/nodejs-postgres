cd8612ef4c3b9c14fad08fac16e3c31495e63c72
theXcelDev <excellence.devsolutions@gmail.com>
2025-05-16 00:55:30 -0300

    chore: setting Prettier up

--- index.js
+++ index.js
@@ -10,20 +10,22 @@ const hostname = encodeURIComponent(process.env.POSTGRES_HOSTNAME)
 const portNumber = encodeURIComponent(process.env.POSTGRES_PORT_NUMBER)
 const database = encodeURIComponent(process.env.POSTGRES_DATABASE)
 
-const sequelize = new Sequelize(`postgres://${username}:${password}@${hostname}:${portNumber}/${database}`)
+const sequelize = new Sequelize(
+  `postgres://${username}:${password}@${hostname}:${portNumber}/${database}`
+)
 
 try {
-    await sequelize.authenticate()
-    console.log('Database connected!')
+  await sequelize.authenticate()
+  console.log('Database connected!')
 } catch (error) {
-    console.error(error)
+  console.error(error)
 }
 
 const port = process.env.PORT_NUMBER || 3001
 const app = express()
 
 app.get('/', (request, response) => {
-    response.send('Hello World!')
+  response.send('Hello World!')
 })
 
 app.listen(port, () => console.log(`Server listening to port ${port}!`))
