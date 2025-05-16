b33634de33960f59318b8930b2c96e1ee36f9641
theXcelDev <excellence.devsolutions@gmail.com>
2025-05-15 23:39:30 -0300

    chore: setting ExpressJs up

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello World!')
+const express = require('express')
+
+const port = 3001
+const app = express()
+
+app.get('/', (request, response) => {
+    response.send('Hello World!')
+})
+
+app.listen(port, () => console.log(`Server listening to port ${port}!`))
