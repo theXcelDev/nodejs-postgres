d4a2baef0e33da23efc3578113b93694dda8c9d1
theXcelDev <excellence.devsolutions@gmail.com>
2025-05-16 00:39:21 -0300

    chore: setting PostgreSQL database connection up with Sequelize

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const dotenv = require('dotenv')
-const express = require('express')
+import dotenv from 'dotenv'
+import express from 'express'
+import { Sequelize } from 'sequelize'
 
 dotenv.config()
+
+const username = encodeURIComponent(process.env.POSTGRES_USERNAME)
+const password = encodeURIComponent(process.env.POSTGRES_PASSWORD)
+const hostname = encodeURIComponent(process.env.POSTGRES_HOSTNAME)
+const portNumber = encodeURIComponent(process.env.POSTGRES_PORT_NUMBER)
+const database = encodeURIComponent(process.env.POSTGRES_DATABASE)
+
+const sequelize = new Sequelize(`postgres://${username}:${password}@${hostname}:${portNumber}/${database}`)
+
+try {
+    await sequelize.authenticate()
+    console.log('Database connected!')
+} catch (error) {
+    console.error(error)
+}
 
 const port = process.env.PORT_NUMBER || 3001
 const app = express()
